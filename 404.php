808224d2321442f4a1c1a4b89eb412da4443aed7
Sarai Chinwag <sarai@saraichinwag.com>
2026-02-05 13:50:17 +0000

    fix(404): Replace emoji with shuffle SVG icon on Surprise Me button
    - Remove sparkle emoji from button text
    - Remove butterfly emoji from intro paragraph
    - Add shuffle SVG icon matching footer style
    - Use proper i18n with __() for button text
    
    Never use emojis in code.

--- 404.php
+++ 404.php
@@ -18,14 +18,19 @@ get_header();
 				</header>
 
 				<div class="entry-content">
-					<p>Oops! The page you're looking for seems to have wandered off. Maybe it's exploring the spiritual meaning of getting lost? 🦋</p>
+					<p>Oops! The page you're looking for seems to have wandered off. Maybe it's exploring the spiritual meaning of getting lost?</p>
 
 					<h2>Try searching:</h2>
 					<?php get_search_form(); ?>
 
 					<h2>Or explore something random:</h2>
 					<div class="wp-block-button">
-						<a class="wp-block-button__link" href="<?php echo esc_url( home_url( '/random-all' ) ); ?>">✨ Surprise Me</a>
+						<a class="wp-block-button__link surprise-me-link" href="<?php echo esc_url( home_url( '/random-all' ) ); ?>">
+							<?php _e('Surprise Me', 'sarai-chinwag'); ?>
+							<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+								<path d="M18 9v-3c-1 0-3.308-.188-4.506 2.216l-4.218 8.461c-1.015 2.036-3.094 3.323-5.37 3.323h-3.906v-2h3.906c1.517 0 2.903-.858 3.58-2.216l4.218-8.461c1.356-2.721 3.674-3.323 6.296-3.323v-3l6 4-6 4zm-9.463 1.324l1.117-2.242c-1.235-2.479-2.899-4.082-5.748-4.082h-3.906v2h3.906c2.872 0 3.644 2.343 4.631 4.324zm15.463 8.676l-6-4v3c-3.78 0-4.019-1.238-5.556-4.322l-1.118 2.241c1.021 2.049 2.1 4.081 6.674 4.081v3l6-4z"/>
+							</svg>
+						</a>
 					</div>
 
 					<h2>Popular reads:</h2>
